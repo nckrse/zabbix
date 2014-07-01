780af2b376f698086e60f95f86c5f4cf806edfde
Friedrich Clausen <friedrich.clausen@blackboard.com>
2014-07-01 14:01:25 +1000

    Don't chown dot files ark!
    * Will merge remove_init_chown once ark PR #63 is merged.

--- recipes/agent_prebuild.rb
+++ recipes/agent_prebuild.rb
@@ -15,6 +15,9 @@ package 'redhat-lsb' if node['platform_family'] == 'rhel'
 ark 'zabbix_agent' do
   name 'zabbix'
   url node['zabbix']['agent']['prebuild']['url']
+  chown_hidden false
+  owner node['zabbix']['agent']['user']
+  group node['zabbix']['agent']['group']
   action :put
   path '/opt'
   strip_components 0
